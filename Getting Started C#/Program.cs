190395c4546e1110558f2abd08175779ba3eb056
Christian Villareal <christian.villareal@mycbscebu.com>
2024-05-16 23:41:51 +0800

    my first commit

--- Getting Started C#/Program.cs
+++ Getting Started C#/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+
+namespace GettingStarted
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my new code!!!");
+            Console.Read();
+        }
+    }
+}
